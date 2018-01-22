2bd21b69282e5f9d23f5cc58ae7984cef659154f
Pongsakorn Somsri <pongsakorns@gmail.com>
2018-01-23 02:15:52 +0700

    Change i18n

--- app/models/konnec_register.rb
+++ app/models/konnec_register.rb
@@ -1,5 +1,5 @@
 class KonnecRegister < ActiveRecord::Base
-	enum university: { "Kasetsart University": 0, "Chulalongkorn University": 1 }
+	enum university: { "เกษตรศาสตร์": 0, "จุฬาลงกรณ์": 1 }
 	enum year: { "1": 1, "2": 2, "3": 3, "4": 4, "other": 0}
 	enum gender: { "Male": 0, "Female": 1, "Others": 2}
 	enum size: { "S อก 34 นิ้ว": 1, "M อก 36 นิ้ว": 2, "L อก 40 นิ้ว": 3, "XL อก 42 นิ้ว": 4, "2XL อก 44 นิ้ว":5, "3XL อก 46 นิ้ว": 6, "4XL อก 48 นิ้ว": 7, "5XL อก 50 นิ้ว": 7 }
