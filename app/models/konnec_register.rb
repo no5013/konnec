69b12874406e42ab6247f2693f0cfcd0090a18b8
Pongsakorn Somsri <pongsakorns@gmail.com>
2018-01-22 20:06:00 +0700

    Add schedule and sponsor content

--- app/models/konnec_register.rb
+++ app/models/konnec_register.rb
@@ -1,7 +1,7 @@
 class KonnecRegister < ActiveRecord::Base
 	enum university: { "Kasetsart University": 0, "Chulalongkorn University": 1 }
 	enum year: { "1": 1, "2": 2, "3": 3, "4": 4, "other": 0}
-	enum size: { xs: 0, s: 1, m: 2, l: 3, xl: 4, "xxl":5, "3xl": 6, "4xl": 7 }
+	enum size: { "S อก 34 นิ้ว": 1, "M อก 36 นิ้ว": 2, "L อก 40 นิ้ว": 3, "XL อก 42 นิ้ว": 4, "2XL อก 44 นิ้ว":5, "3XL อก 46 นิ้ว": 6, "4XL อก 48 นิ้ว": 7, "5XL อก 50 นิ้ว": 7 }
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :nickname, presence: true
